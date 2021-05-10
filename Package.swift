40573b308593b98262591b9231072225f9021c6c
Kevin Mullins <kevin_mullins@appracatappra.com>
2021-05-10 10:40:53 -0500

    Moved Documentation
    Moved documentation to remove it from the build targets.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftletData",
-            dependencies: []),
+            dependencies: [],
+            exclude:["Documentation"]),
         .testTarget(
             name: "SwiftletDataTests",
             dependencies: ["SwiftletData"]),
